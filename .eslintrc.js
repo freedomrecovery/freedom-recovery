7de6072aa9986a39a0813899c23e3c63d6780302
CherylMelanson <cherylmelanson@threeflow.com>
2024-07-05 14:12:03 -0400

    updates for vue 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es2021: true
   },
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/eslint-config-standard'
+    'plugin:vue/vue3-essential',
+    '@vue/standard'
   ],
   parserOptions: {
     parser: '@babel/eslint-parser',
@@ -17,3 +17,4 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
+
